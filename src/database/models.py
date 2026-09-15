68903dfff7cf724bdc8ff72fc1381f1d44bf755f
777Afanas <serhii2111@yahoo.com>
2026-09-15 22:35:27 +0300

    Add additional_data to Contact model and apply migration

--- src/database/models.py
+++ src/database/models.py
@@ -1,20 +1,21 @@
 from datetime import date
 from typing import Optional
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Date, String, Integer
-
-
-class Base(DeclarativeBase):
-    pass
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Date, String, Integer, Text
+from src.database.db import Base
 
 
 class Contact(Base):
     __tablename__ = "contacts"
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    first_name: Mapped[str] = mapped_column(String(50))
-    last_name: Mapped[str] = mapped_column(String(50))
-    email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
-    phone: Mapped[str] = mapped_column(String(20))
-    # Ось тут ключовий момент:
-    birthday: Mapped[Optional[date]] = mapped_column(Date, nullable=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(100), unique=True, index=True, nullable=False
+    )
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    birthday: Mapped[date] = mapped_column(Date, nullable=False)
+    additional_data: Mapped[Optional[str]] = mapped_column(
+        Text, nullable=True, default=None
+    )
